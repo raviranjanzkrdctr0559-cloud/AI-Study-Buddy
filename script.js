73c784f35215753371692ec7f89170b6116fb495
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-20 14:05:13 +0530

    study app

--- script.js
+++ script.js
@@ -10,63 +10,63 @@ return;
 
 answer.innerHTML = "Searching...";
 
-try {
+try{
 
 let response = await fetch(
-`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+https://api.dictionaryapi.dev/api/v2/entries/en/${word}
 );
 
 let data = await response.json();
 
 let meaning =
-data?.[0]?.meanings?.[0]?.definitions?.[0]?.definition ||
-"No meaning found";
+data[0].meanings[0].definitions[0].definition;
 
 let translateResponse = await fetch(
-`https://api.mymemory.translated.net/get?q=${word}&langpair=en|hi`
+https://api.mymemory.translated.net/get?q=${word}&langpair=en|hi
 );
 
 let translateData = await translateResponse.json();
 
 let hindiMeaning =
-translateData?.responseData?.translatedText ||
-"No translation available";
+translateData.responseData.translatedText;
 
-answer.innerHTML = `
+answer.innerHTML = 
 <h3>${word}</h3>
-<p><b>English Meaning:</b><br>${meaning}</p>
-<p><b>Hindi Translation:</b><br>${hindiMeaning}</p>
-`;
 
-} catch (error) {
-console.log(error);
-answer.innerHTML = "❌ Meaning not found or API error";
+<p><b>English Meaning:</b><br>${meaning}</p>
+
+<p><b>Hindi Translation:</b><br>${hindiMeaning}</p>
+;
+
+}
+catch{
+
+answer.innerHTML =
+"❌ Meaning not found.";
 }
 
 }
 function startVoice() {
 
-if (!("SpeechRecognition" in window || "webkitSpeechRecognition" in window)) {
-alert("Voice not supported in this browser");
-return;
-}
-
 const recognition =
-new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+new(window.SpeechRecognition ||
+window.webkitSpeechRecognition)();
 
 recognition.lang = "en-US";
 
 recognition.start();
 
 recognition.onresult = function(event) {
+
 document.getElementById("word").value =
 event.results[0][0].transcript;
+
 };
 
 }
 function speakWord() {
 
-let word = document.getElementById("word").value.trim();
+let word = document.getElementById("word").value;
 
 if(word === ""){
 alert("Please enter a word.");
@@ -74,8 +74,9 @@ return;
 }
 
 let speech = new SpeechSynthesisUtterance(word);
+
 speech.lang = "en-US";
 
 window.speechSynthesis.speak(speech);
 
-}
+} 
